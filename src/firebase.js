d89e87eb8da5fe83ad1014d6b520e32a166383a0
Saket Sultania <41504377+SAKET03@users.noreply.github.com>
2021-04-14 00:09:56 +0530

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyCjc_p6uydgKvYAstsE7i1YC6LO5u5NBA8',
-  authDomain: 'chat-app-e9aa2.firebaseapp.com',
-  databaseURL: 'https://chat-app-e9aa2-default-rtdb.firebaseio.com',
-  storageBucket: 'chat-app-e9aa2.appspot.com'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: ''
 };
 
 firebase.initializeApp(config);
